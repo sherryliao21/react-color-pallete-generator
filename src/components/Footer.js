6fd2f6ecedf1c6d92de0d4c678af39ee09dbc2d9
sherryliao21 <sherry.c.liao@gmail.com>
2021-08-05 00:47:38 +0800

    feat: add referrer settings to link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div class="footer">
       <p class="footer-text">Made with
       <span class="text-red-400 mx-2">&#9829;</span>
-        by Sherry Liao &copy; 2021 <a href="https://github.com/sherryliao21" target="_blank"><FontAwesomeIcon icon={ faGithub } class="github-logo"/></a></p>
+        by Sherry Liao &copy; 2021 <a href="https://github.com/sherryliao21" target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={ faGithub } class="github-logo"/></a></p>
     </div>
   )
 }
